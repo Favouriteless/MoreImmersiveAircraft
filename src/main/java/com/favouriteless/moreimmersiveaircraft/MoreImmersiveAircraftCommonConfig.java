6c097abd34d7f9914f4f4fcbcd1428b4c93a79f4
Favouriteless <62647254+Favouriteless@users.noreply.github.com>
2023-08-29 00:31:42 +0100

    Initial commit

---
+++ src/main/java/com/favouriteless/moreimmersiveaircraft/MoreImmersiveAircraftCommonConfig.java
@@ -0,0 +1,30 @@
+package com.favouriteless.moreimmersiveaircraft;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+@EventBusSubscriber(modid=MoreImmersiveAircraft.MODID, bus= Bus.MOD)
+public class MoreImmersiveAircraftCommonConfig {
+
+	public static final ForgeConfigSpec SPEC;
+
+	public static final ConfigValue<Boolean> ENABLE_CRASH_EXPLOSION;
+	public static final ConfigValue<Double> CRASH_EXPLOSION_RADIUS;
+	public static final ConfigValue<Boolean> CRASH_EXPLOSION_FIRE;
+
+
+	static {
+		ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+
+		builder.push("Engined Vehicle Crashes");
+		ENABLE_CRASH_EXPLOSION = builder.comment("Enable engined vehicles exploding when they crash. #default true").define("enable_crash_explosion", true);
+		CRASH_EXPLOSION_RADIUS = builder.comment("Explosion radius for crashing vehicles. #default 2.0").define("crash_explosion_radius", 2.0D);
+		CRASH_EXPLOSION_FIRE = builder.comment("Crash explosions generate fire. #default true").define("crash_explosion_fire", true);
+		builder.pop();
+
+		SPEC = builder.build();
+	}
+
+}
---
+++ src/main/java/com/favouriteless/moreimmersiveaircraft/mixin/MixinVehicleEntity.java
@@ -0,0 +1,27 @@
+package com.favouriteless.moreimmersiveaircraft.mixin;
+
+import com.favouriteless.moreimmersiveaircraft.MoreImmersiveAircraftCommonConfig;
+import immersive_aircraft.entity.EngineAircraft;
+import immersive_aircraft.entity.VehicleEntity;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level.ExplosionInteraction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(VehicleEntity.class)
+public class MixinVehicleEntity {
+
+	@Inject(method = "hurt", at=@At(value = "INVOKE", target = "Limmersive_aircraft/entity/VehicleEntity;discard()V"))
+	private void hurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+		if(MoreImmersiveAircraftCommonConfig.ENABLE_CRASH_EXPLOSION.get() && !(source.getEntity() instanceof Player) && ((VehicleEntity)(Object)this) instanceof EngineAircraft aircraft) {
+			aircraft.level().explode(aircraft, aircraft.getX(), aircraft.getY(), aircraft.getZ(),
+					MoreImmersiveAircraftCommonConfig.CRASH_EXPLOSION_RADIUS.get().floatValue(),
+					MoreImmersiveAircraftCommonConfig.CRASH_EXPLOSION_FIRE.get(),
+					ExplosionInteraction.MOB);
+		}
+	}
+
+}
